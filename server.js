28786a9d858d322e32187c93f4f9f3fef76f7f82
natanrubenich-source <natan.rubenich@edu.sc.senai.br>
2026-07-16 18:58:03 -0300

    descartar dps

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import db from './models/index.js'; 
 import { autenticar, somenteAdmin } from './middleware/auth.js';
 import * as authController from './controllers/authController.js';
+import * as avaliacaoController from './controllers/avaliacaoController.js';
+
 const app = express();
 app.use(cors()); // Permite cross plataform para utilizar o client e server em localhost
 app.use(express.json()); // Permite leitura de JSON
@@ -17,6 +19,14 @@ app.post('/registrar',
 // Rota pública: login e geração do token
 app.post('/login', authController.login);
 
+app.post('/avaliacao', (req, res, next) => {
+    req.setTimeout(900000);
+    if (req.socket) {
+        req.socket.setTimeout(900000); // Garante que o canal TCP não feche por inatividade
+    }
+    next();
+}, avaliacaoController.gerar);
+
 // Rota protegida: acessível para qualquer usuário autenticado
 app.get('/painel', autenticar, (req, res) => {
   res.send(`Olá, ${req.usuario.nome}. Seu cargo é: ${req.usuario.cargo}`);
@@ -24,6 +34,8 @@ app.get('/painel', autenticar, (req, res) => {
 
 // Sincroniza os modelos com o banco e inicia o servidor
 db.sequelize.sync().then(() => {
-  app.listen(5000, () => console.log("Servidor da clínica rodando na porta 5000"));
+const server =  app.listen(5000, () => console.log("Servidor rodando na porta 5000"));
+server.requestTimeout = 900000; 
+server.headersTimeout = 905000; 
 })
 .catch(err => console.log('Error: ' + err));
